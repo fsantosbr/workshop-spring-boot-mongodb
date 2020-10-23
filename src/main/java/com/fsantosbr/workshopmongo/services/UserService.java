275ef82f38002778461e7baaa6f98bb3234b95c8
Fabio Alves <fsantosinfo@gmail.com>
2020-10-22 22:32:18 -0300

    Inserção de usuário com POST

--- src/main/java/com/fsantosbr/workshopmongo/services/UserService.java
+++ src/main/java/com/fsantosbr/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fsantosbr.workshopmongo.domain.User;
+import com.fsantosbr.workshopmongo.dto.UserDTO;
 import com.fsantosbr.workshopmongo.repository.UserRepository;
 import com.fsantosbr.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
 
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
