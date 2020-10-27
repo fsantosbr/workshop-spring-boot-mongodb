d330f2ac9b69b7109645104bd64d53be5c07a5dc
Fabio Alves <fsantosinfo@gmail.com>
2020-10-26 22:30:58 -0300

    Consulta simples com @Query

--- src/main/java/com/fsantosbr/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fsantosbr/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.fsantosbr.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fsantosbr.workshopmongo.domain.Post;
@@ -14,6 +15,14 @@ import com.fsantosbr.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	// The Spring data has (already) the MongoRepository<T, ID> interface
 	// The interface expects the domain class it will manage and the type of the Id of the class	
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	/* - Above is the MongoDb query. It's in Json format.
+	 * title = the field name.
+	 * ?0 = the first parameter in the method (text).
+	 * 'i' = the option from mongodb kb. Here, works to ignore case sensitive.
+	 */
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);	
 	/*	- Em nenhum momento precisamos implementar o corpo deste método.
--- src/main/java/com/fsantosbr/workshopmongo/services/PostService.java
+++ src/main/java/com/fsantosbr/workshopmongo/services/PostService.java
@@ -23,6 +23,6 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle (String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
 	}
 }
